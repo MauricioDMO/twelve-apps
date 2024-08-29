77b881ea2543fcd4e59ba510baa67812634708c6
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-08-29 17:41:44 -0600

    Add font

--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -1,7 +1,9 @@
-import { Roboto_Mono } from "next/font/google"
+import { Roboto_Mono, Inter } from "next/font/google"
 
 export const robotoMono = Roboto_Mono({
   weight: ["200", "400", "700"],
   subsets: ["latin"],
   
 })
+
+export const inter = Inter({ subsets: ['latin'] })
