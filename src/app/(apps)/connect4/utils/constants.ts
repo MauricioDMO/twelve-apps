2d8c04c3f718a61f17dd59439443468e699e9c00
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-09-05 19:50:27 -0600

    Add constants for tokens and colors in Connect4 game

---
+++ src/app/(apps)/connect4/utils/constants.ts
@@ -0,0 +1,31 @@
+export const TOKEN  = {
+  EMPTY: 0,
+  PLAYER1: 1,
+  PLAYER2: 2
+}
+
+export const redColors = [
+  "#FF0000",
+  "#FF6347",
+  "#DC143C",
+  "#B22222",
+  "#FF4500",
+  "#CD5C5C",
+  "#8B0000",
+  "#FA8072",
+  "#E9967A",
+  "#FF7F7F"
+];
+
+export const yellowColors = [
+  "#FFFF00",
+  "#FFD700",
+  "#FFFACD",
+  "#FFEFD5",
+  "#FFE4B5",
+  "#FFDAB9",
+  "#F0E68C",
+  "#FFFFE0",
+  "#FFFAF0",
+  "#FFF8DC"
+];
