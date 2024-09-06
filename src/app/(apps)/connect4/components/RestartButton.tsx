3c891fe470cf0f4e131489bb431bcf441e0b4fb7
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-09-05 19:56:19 -0600

    Add button to restart the game

---
+++ src/app/(apps)/connect4/components/RestartButton.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useGame } from "../hooks/useGame";
+
+export function RestartButton() {
+  const { resetGame } = useGame()
+
+  return (
+    <button
+      onClick={resetGame}
+      className="px-4 py-2 my-4 bg-zinc-600 text-white rounded">
+      Reiniciar
+    </button>
+  );
+}
