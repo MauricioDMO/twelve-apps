9b26736267ece7ac74a911ef0ec45bf424838eca
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-09-13 21:38:18 -0600

    Add HelpBubble component for displaying a help dialog with game instructions and controls and add a new background

--- src/app/(apps)/connect4/page.tsx
+++ src/app/(apps)/connect4/page.tsx
@@ -6,6 +6,7 @@ import { Table } from "./components/Table";
 import { GameProvider } from "./context/Game";
 import { Panel } from "./components/Panel";
 import { ResetWinsButton } from "./components/ResetWinsButton";
+import { HelpBubble } from "./components/HelpBubble";
  
 export const metadata: Metadata = {
   title: '12 Apps - Conecta 4',
@@ -17,19 +18,22 @@ export default function Connect4 () {
   return (
     <>
       <GlobalCss />
-      <h1 className={`text-2xl py-4 text-center ${robotoMono.className}`} >
-        Conecta 4
-      </h1>
-      <section className="w-full px-4 place-items-center grid">
-      <GameProvider>
-        <Panel />
-        <Table />
-        <section className="flex gap-2 my-8">
-          <RestartButton />
-          <ResetWinsButton />
+      <div className="bg-gradient-to-br from-slate-700 via-slate-900 to-black">
+        <h1 className={`text-2xl py-4 text-center ${robotoMono.className}`} >
+          Conecta 4
+        </h1>
+        <section className="w-full px-4 place-items-center grid">
+          <GameProvider>
+            <Panel />
+            <Table />
+            <section className="flex gap-2 my-8">
+              <RestartButton />
+              <ResetWinsButton />
+            </section>
+          </GameProvider>
         </section>
-      </GameProvider>
-      </section>
+        <HelpBubble />
+      </div>
     </>
   )
 }
