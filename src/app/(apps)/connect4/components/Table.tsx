4c4bd0b123d799ca0d160a06dffe8d3a70b49399
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-09-13 21:36:41 -0600

    Add handle keyboard input for gameplay

--- src/app/(apps)/connect4/components/Table.tsx
+++ src/app/(apps)/connect4/components/Table.tsx
@@ -1,11 +1,29 @@
 "use client"
 
+import { useEffect } from "react"
 import { useGame } from "../hooks/useGame"
 import style from "./Table.module.css"
 import { Token } from "./Token"
 
 export function Table () {
-  const { table } = useGame()
+  const { table, setCell, resetGame } = useGame()
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if ("1234567".includes(event.key)) {
+        setCell(+event.key - 1)
+      }
+      else if (event.key === " ") {
+        event.preventDefault()
+        resetGame()
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [setCell, resetGame])
+
+
 
   return (
     <section className="relative size-full aspect-[7/6] max-w-md">
