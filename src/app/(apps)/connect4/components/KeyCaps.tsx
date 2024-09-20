252b5c067a9da6e85da0e609c567d40810f66120
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-09-20 17:03:57 -0600

    Refactor KeyCaps component to use hints from useHints hook

--- src/app/(apps)/connect4/components/KeyCaps.tsx
+++ src/app/(apps)/connect4/components/KeyCaps.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import { robotoMono } from "@/ui/fonts"
 import { Key } from "./Key"
+import { useHints } from "../hooks/useHints"
 
 export function KeyCaps () {
+  const { hints } = useHints()
+
   return (
     <section className={`h-16 py-2 gap-2 grid grid-cols-7 w-full max-w-md ${robotoMono.className}`}>
-      {Array.from({ length: 7 }, (_, i) => (
+      {hints && Array.from({ length: 7 }, (_, i) => (
         <Key key={i} className="text-slate-500/80 size-full"
         fontWeight={700} fontClassName={robotoMono.className}
         text={`${i + 1}`} keyTrigger={`${i + 1}`} />
